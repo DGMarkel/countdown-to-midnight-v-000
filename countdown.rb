0db65b266e5461a95b08cac7accfa100cdc433a3
David Markel <davidgmarkel@gmail.com>
2018-03-09 02:12:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,11 @@ def countdown(number)
   end
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
+  while number > 0
+    number -= 1
+      puts "#{number} SECOND(S)!"
+      sleep 1    
 end
 
 countdown(3)
